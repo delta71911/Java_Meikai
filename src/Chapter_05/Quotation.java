a838776ea23b3f21fea1337e330b8988bc361894
delta71911 <delta71911@naver.com>
2023-04-15 18:58:32 +0900

    2023-04-15

---
+++ src/Chapter_05/Quotation.java
@@ -0,0 +1,11 @@
+package Chapter_05;
+
+public class Quotation {
+    public static void main(String[] args) {
+        System.out.println("이중인용괄호로 쌓여진\"ABC\"는 문자열 리터럴입니다.");
+
+        System.out.print("일중인용괄호로 둘러싸인");
+        System.out.print('\'');
+        System.out.println("A는 문자리터럴입니다.");
+    }
+}
